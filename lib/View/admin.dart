5e9489e43b07d3dde6b3722fefca9e038cb158a4
Ilham AJ <purplefinneptuna@gmail.com>
2024-02-11 16:42:11 +0700

    Fix missing list item
    gegara kena batas

--- lib/View/admin.dart
+++ lib/View/admin.dart
@@ -6,7 +6,6 @@ import 'package:projectuas/Model/modelpegawai.dart';
 import 'package:projectuas/View/editpegawai.dart';
 import 'package:projectuas/View/login.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:logger/logger.dart';
 
 class Admin extends StatefulWidget {
   const Admin({super.key});
@@ -18,7 +17,6 @@ class Admin extends StatefulWidget {
 class _AdminState extends State<Admin> {
   late CollectionReference _pegawai;
   late Query _karyawan;
-  final Logger logger = Logger();
 
   @override
   void initState() {
@@ -52,107 +50,109 @@ class _AdminState extends State<Admin> {
           child: SingleChildScrollView(
             child: Column(children: [
               SizedBox(
-                  height: 500,
                   child: StreamBuilder(
-                    stream: _karyawan.snapshots(),
-                    builder: (context, AsyncSnapshot snapshots) {
-                      if (snapshots.connectionState == ConnectionState.waiting) {
-                        return const Center(
-                          child: CircularProgressIndicator(color: Colors.green),
-                        );
-                      }
-                      if (snapshots.hasData) {
-                        return ListView.builder(
-                            itemCount: snapshots.data!.docs.length,
-                            itemBuilder: (context, index) {
-                              final DocumentSnapshot records = snapshots.data!.docs[index];
-                              return Padding(
-                                  padding: const EdgeInsets.symmetric(vertical: 5),
-                                  child: Slidable(
-                                    startActionPane: ActionPane(
-                                      motion: const StretchMotion(),
-                                      children: [
-                                        SlidableAction(
-                                          onPressed: (context) {
-                                            final pgw = UserModel(
-                                              id: records.id,
-                                              nama: records["nama"],
-                                              posisi: records["posisi"],
-                                              gajipokok: records["gajipokok"],
-                                              uangmakan: records["uangmakan"],
-                                              izin: records["izin"],
-                                              rool: records["rool"],
+                stream: _karyawan.snapshots(includeMetadataChanges: true),
+                builder: (context, AsyncSnapshot snapshots) {
+                  if (snapshots.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(color: Colors.green),
+                    );
+                  }
+                  if (snapshots.hasData) {
+                    var documents = snapshots.data.docs;
+                    return ListView.builder(
+                        shrinkWrap: true,
+                        key: UniqueKey(),
+                        itemCount: documents.length,
+                        itemBuilder: (context, index) {
+                          final DocumentSnapshot records = documents[index];
+                          return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 5),
+                              child: Slidable(
+                                startActionPane: ActionPane(
+                                  motion: const StretchMotion(),
+                                  children: [
+                                    SlidableAction(
+                                      onPressed: (context) {
+                                        final pgw = UserModel(
+                                          id: records.id,
+                                          nama: records["nama"],
+                                          posisi: records["posisi"],
+                                          gajipokok: records["gajipokok"],
+                                          uangmakan: records["uangmakan"],
+                                          izin: records["izin"],
+                                          rool: records["rool"],
+                                        );
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: ((context) => Managepgw(
+                                                      mpegawai: pgw,
+                                                      index: index,
+                                                    ))));
+                                      },
+                                      icon: Icons.edit_note,
+                                      backgroundColor: Colors.yellowAccent,
+                                    )
+                                  ],
+                                ),
+                                endActionPane: ActionPane(
+                                  motion: const StretchMotion(),
+                                  children: [
+                                    SlidableAction(
+                                      onPressed: (context) {
+                                        showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            return AlertDialog(
+                                              title: const Text('Konfirmasi'),
+                                              content: const Text('Apakah anda yakin ingin menghapus data karyawan ini?'),
+                                              actions: [
+                                                TextButton(
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+                                                    Pegawai().hapusPegawai(UserModel(
+                                                      id: records.id,
+                                                      nama: null,
+                                                      posisi: null,
+                                                      gajipokok: null,
+                                                      uangmakan: null,
+                                                      izin: null,
+                                                      rool: null,
+                                                    ));
+                                                  },
+                                                  child: const Text('Confirm'),
+                                                ),
+                                                TextButton(
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+                                                  },
+                                                  child: const Text('Cancel'),
+                                                ),
+                                              ],
                                             );
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: ((context) => Managepgw(
-                                                          mpegawai: pgw,
-                                                          index: index,
-                                                        ))));
                                           },
-                                          icon: Icons.edit_note,
-                                          backgroundColor: Colors.yellowAccent,
-                                        )
-                                      ],
-                                    ),
-                                    endActionPane: ActionPane(
-                                      motion: const StretchMotion(),
-                                      children: [
-                                        SlidableAction(
-                                          onPressed: (context) {
-                                            showDialog(
-                                              context: context,
-                                              builder: (BuildContext context) {
-                                                return AlertDialog(
-                                                  title: const Text('Konfirmasi'),
-                                                  content: const Text('Apakah anda yakin ingin menghapus data karyawan ini?'),
-                                                  actions: [
-                                                    TextButton(
-                                                      onPressed: () {
-                                                        Navigator.of(context).pop();
-                                                        Pegawai().hapusPegawai(UserModel(
-                                                          id: records.id,
-                                                          nama: null,
-                                                          posisi: null,
-                                                          gajipokok: null,
-                                                          uangmakan: null,
-                                                          izin: null,
-                                                          rool: null,
-                                                        ));
-                                                      },
-                                                      child: const Text('Confirm'),
-                                                    ),
-                                                    TextButton(
-                                                      onPressed: () {
-                                                        Navigator.of(context).pop();
-                                                      },
-                                                      child: const Text('Cancel'),
-                                                    ),
-                                                  ],
-                                                );
-                                              },
-                                            );
-                                          },
-                                          icon: Icons.delete_outline,
-                                          backgroundColor: Colors.redAccent,
-                                        )
-                                      ],
-                                    ),
-                                    child: ListTile(
-                                      tileColor: Colors.blueAccent,
-                                      title: Text(records["nama"]),
-                                      subtitle: Text(records["posisi"]),
-                                    ),
-                                  ));
-                            });
-                      } else {
-                        return const Center(
-                          child: CircularProgressIndicator(color: Colors.green),
-                        );
-                      }
-                    },
-                  ))
+                                        );
+                                      },
+                                      icon: Icons.delete_outline,
+                                      backgroundColor: Colors.redAccent,
+                                    )
+                                  ],
+                                ),
+                                child: ListTile(
+                                  tileColor: Colors.blueAccent,
+                                  title: Text(records["nama"]),
+                                  subtitle: Text(records["posisi"]),
+                                ),
+                              ));
+                        });
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(color: Colors.green),
+                    );
+                  }
+                },
+              ))
             ]),
           ),
         ));
